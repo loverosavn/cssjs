7394e629246ad5c15288cad658732eb2b1ae5e40
loverosavn <112917108+loverosavn@users.noreply.github.com>
2022-09-07 21:33:57 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://loverosa.vn Muốn sử dụng thì dán code này lên web nè <script src='https://raw.githack.com/loverosavn/cssjs/main/redirectcopy.js' type='text/javascript'/>'>https://loverosa.vn</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://loverosa.vn'>https://loverosa.vn</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
